a6ede6a2785ab9492c1ac9136bfed6c5ce26b02c
Devpopp <162092815+Devpopp@users.noreply.github.com>
2024-03-14 20:56:00 +0530

    Update Componenet.ts

--- Componenet.ts
+++ Componenet.ts
@@ -1,3 +1,127 @@
+import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [AgGridAngular],
+  template: `<ag-grid-angular
+    style="width: 100%; height: 100%;"
+    [rowData]="rowData"
+    [columnDefs]="columnDefs"
+    [defaultColDef]="defaultColDef"
+    [rowSelection]="rowSelection"
+    [suppressRowClickSelection]="true"
+    [pagination]="true"
+    [paginationPageSize]="paginationPageSize"
+    [paginationPageSizeSelector]="paginationPageSizeSelector"
+    [class]="themeClass"
+  ></ag-grid-angular>`,
+})
+export class AppComponent {
+  public rowData: any[] | null = [
+    {
+      make: 'Tesla',
+      model: 'Model Y',
+      price: 64950,
+      electric: true,
+      month: 'June',
+    },
+    {
+      make: 'Ford',
+      model: 'F-Series',
+      price: 33850,
+      electric: false,
+      month: 'October',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: false,
+      month: 'August',
+    },
+    {
+      make: 'Mercedes',
+      model: 'EQA',
+      price: 48890,
+      electric: true,
+      month: 'February',
+    },
+    {
+      make: 'Fiat',
+      model: '500',
+      price: 15774,
+      electric: false,
+      month: 'January',
+    },
+    {
+      make: 'Nissan',
+      model: 'Juke',
+      price: 20675,
+      electric: false,
+      month: 'March',
+    },
+    {
+      make: 'Vauxhall',
+      model: 'Corsa',
+      price: 18460,
+      electric: false,
+      month: 'July',
+    },
+    {
+      make: 'Volvo',
+      model: 'EX30',
+      price: 33795,
+      electric: true,
+      month: 'September',
+    },
+    {
+      make: 'Mercedes',
+      model: 'Maybach',
+      price: 175720,
+      electric: false,
+      month: 'December',
+    },
+    {
+      make: 'Vauxhall',
+      model: 'Astra',
+      price: 25795,
+      electric: false,
+      month: 'April',
+    },
+    {
+      make: 'Fiat',
+      model: 'Panda',
+      price: 13724,
+      electric: false,
+      month: 'November',
+    },
+    {
+      make: 'Jaguar',
+      model: 'I-PACE',
+      price: 69425,
+      electric: true,
+      month: 'May',
+    },
+  ];
+  public columnDefs: ColDef[] = [
+    {
+      field: 'make',
+      checkboxSelection: true,
+      editable: true,
+      cellEditor: 'agSelectCellEditor',
+      cellEditorParams: {
+        values: [
+          'Tesla',
+          'Ford',
+          'Toyota',
+          'Mercedes',
+          'Fiat',
+          'Nissan',
           'Vauxhall',
           'Volvo',
           'Jaguar',
